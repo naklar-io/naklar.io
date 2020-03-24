108d954d65424f96b2964da0ff7217a3937da9e7
Max Fruehauf <fruehaufmaximilian@gmail.com>
2020-03-24 22:28:21 +0100

    add more components

--- frontend/naklario/src/app/app.module.ts
+++ frontend/naklario/src/app/app.module.ts
@@ -1,15 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
+
+// Third party modules
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+
+// Components
 import { AppComponent } from './app.component';
+import { RouletteComponent } from './roulette/roulette.component';
+import { AccountComponent } from './account/account.component';
+import { CallComponent } from './call/call.component';
+import { HomeComponent } from './home/home.component';
+import { LandingComponent } from './landing/landing.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RouletteComponent,
+    AccountComponent,
+    CallComponent,
+    HomeComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
+    NgbModule,
     AppRoutingModule
   ],
   providers: [],
