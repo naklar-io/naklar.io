8dff4186ed81c80b605cc83154506d55932fd374
Korbinian Stein <kstein@inforbi.de>
2020-03-25 00:16:30 +0100

    Add example routing

--- frontend/naklario/src/app/app-routing.module.ts
+++ frontend/naklario/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './home/home.component';
+import { AccountComponent } from './account/account.component';
 
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path: 'account', component: AccountComponent}
 ];
 
 @NgModule({
