39ed95ca126ccfa95ffc9cc52b17ce78890aad86
Max Fruehauf <fruehaufmaximilian@gmail.com>
2020-03-26 16:14:56 +0100

    Add basic landing page

--- frontend/naklario/src/app/app-routing.module.ts
+++ frontend/naklario/src/app/app-routing.module.ts
@@ -4,9 +4,11 @@ import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { AccountComponent } from "./account/account.component";
 import { PageNotFoundComponent } from "./misc/page-not-found/page-not-found.component";
+import { LandingComponent } from "./landing/landing.component";
 
 const routes: Routes = [
   { path: "account", component: AccountComponent },
+  { path: "landing", component: LandingComponent },
   { path: "", component: HomeComponent, pathMatch: "full" },
   { path: "**", component: PageNotFoundComponent }
 ];
