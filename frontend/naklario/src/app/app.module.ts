a60421c35bdd2e97a305a0d4b78d4fae1a4c1a2b
Max Fruehauf <fruehaufmaximilian@gmail.com>
2020-03-26 17:46:50 +0100

    Add landing/email form

--- frontend/naklario/src/app/app.module.ts
+++ frontend/naklario/src/app/app.module.ts
@@ -1,21 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { AppRoutingModule } from './app-routing.module';
-import {ReactiveFormsModule } from '@angular/forms'
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppRoutingModule } from "./app-routing.module";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule, HttpClient } from "@angular/common/http";
 
 // Third party modules
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-
+import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 
 // Components
-import { AppComponent } from './app.component';
-import { RouletteComponent } from './roulette/roulette.component';
-import { AccountComponent } from './account/account.component';
-import { CallComponent } from './call/call.component';
-import { HomeComponent } from './home/home.component';
-import { LandingComponent } from './landing/landing.component';
-import { NavbarComponent } from './misc/navbar/navbar.component';
-import { PageNotFoundComponent } from './misc/page-not-found/page-not-found.component';
+import { AppComponent } from "./app.component";
+import { RouletteComponent } from "./roulette/roulette.component";
+import { AccountComponent } from "./account/account.component";
+import { CallComponent } from "./call/call.component";
+import { HomeComponent } from "./home/home.component";
+import { LandingComponent } from "./landing/landing.component";
+import { NavbarComponent } from "./misc/navbar/navbar.component";
+import { PageNotFoundComponent } from "./misc/page-not-found/page-not-found.component";
 
 @NgModule({
   declarations: [
@@ -28,13 +28,8 @@ import { PageNotFoundComponent } from './misc/page-not-found/page-not-found.comp
     NavbarComponent,
     PageNotFoundComponent
   ],
-  imports: [
-    NgbModule,
-    BrowserModule,
-    ReactiveFormsModule,
-    AppRoutingModule
-  ],
+  imports: [NgbModule, BrowserModule,HttpClientModule,  AppRoutingModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
