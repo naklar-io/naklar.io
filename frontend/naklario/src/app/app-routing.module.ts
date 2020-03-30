981bb1fda6e3d8125654c1a86c06721e33f9c91b
Julian Rixner <julirix@hotmail.de>
2020-03-30 21:43:43 +0200

    impressum and about

--- frontend/naklario/src/app/app-routing.module.ts
+++ frontend/naklario/src/app/app-routing.module.ts
@@ -5,10 +5,15 @@ import { HomeComponent } from "./home/home.component";
 import { AccountComponent } from "./account/account.component";
 import { PageNotFoundComponent } from "./misc/page-not-found/page-not-found.component";
 import { LandingComponent } from "./landing/landing.component";
+import { ImpressumComponent } from "./impressum/impressum.component";
+import { AboutComponent } from "./about/about.component";
+
 
 const routes: Routes = [
   { path: "account", component: AccountComponent },
   { path: "landing", component: LandingComponent },
+  { path: "impressum", component: ImpressumComponent },
+  { path: "about", component: AboutComponent },
   { path: "", component: HomeComponent, pathMatch: "full" },
   { path: "**", component: PageNotFoundComponent }
 ];
