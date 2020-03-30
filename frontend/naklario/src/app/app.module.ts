9c37390648616aba0a8c4241cf5dc842cdab7d9e
Max Fruehauf <fruehaufmaximilian@gmail.com>
2020-03-30 17:19:55 +0200

    add account module

--- frontend/naklario/src/app/app.module.ts
+++ frontend/naklario/src/app/app.module.ts
@@ -10,25 +10,33 @@ import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
 // Components
 import { AppComponent } from "./app.component";
 import { RouletteComponent } from "./roulette/roulette.component";
-import { AccountComponent } from "./account/account.component";
 import { CallComponent } from "./call/call.component";
 import { HomeComponent } from "./home/home.component";
 import { LandingComponent } from "./landing/landing.component";
 import { NavbarComponent } from "./misc/navbar/navbar.component";
 import { PageNotFoundComponent } from "./misc/page-not-found/page-not-found.component";
+import { AccountModule } from "./account/account.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     RouletteComponent,
-    AccountComponent,
     CallComponent,
     HomeComponent,
     LandingComponent,
     NavbarComponent,
     PageNotFoundComponent
   ],
-  imports: [NgbModule, BrowserModule,HttpClientModule,  AppRoutingModule, ReactiveFormsModule],
+  imports: [
+    NgbModule,
+    BrowserModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    // modules (arbitrary order)
+    AccountModule,
+    // AppRoutingComponent needs to be the last routing module
+    AppRoutingModule,
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
