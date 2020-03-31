ee0693607c1592de55c095ef409273dd86b7cdde
Max Fruehauf <fruehaufmaximilian@gmail.com>
2020-03-31 23:35:13 +0200

    paritially add tutor register form

--- frontend/naklario/src/app/account/account.module.ts
+++ frontend/naklario/src/app/account/account.module.ts
@@ -5,11 +5,13 @@ import { AccountRoutingModule } from './account-routing.module';
 import { TutorComponent } from './tutor/tutor.component';
 import { StudentComponent } from './student/student.component';
 import { TutorRegisterComponent } from './tutor/tutor-register/tutor-register.component';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [TutorComponent, StudentComponent, TutorRegisterComponent],
   imports: [
+    FormsModule,
     CommonModule,
     AccountRoutingModule
   ]
