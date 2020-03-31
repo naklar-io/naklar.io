09d686dcebf61b724a7c0bc1a122d441f6d19306
Max Fruehauf <fruehaufmaximilian@gmail.com>
2020-03-31 22:34:40 +0200

    Add GlobalStateService

--- frontend/naklario/src/app/account/account.module.ts
+++ frontend/naklario/src/app/account/account.module.ts
@@ -4,10 +4,11 @@ import { CommonModule } from '@angular/common';
 import { AccountRoutingModule } from './account-routing.module';
 import { TutorComponent } from './tutor/tutor.component';
 import { StudentComponent } from './student/student.component';
+import { TutorRegisterComponent } from './tutor/tutor-register/tutor-register.component';
 
 
 @NgModule({
-  declarations: [TutorComponent, StudentComponent],
+  declarations: [TutorComponent, StudentComponent, TutorRegisterComponent],
   imports: [
     CommonModule,
     AccountRoutingModule
