9628545384de8b8c17a5419a8de7e18366aee3a7
Max Fruehauf <fruehaufmaximilian@gmail.com>
2020-03-31 16:58:15 +0200

    make forms template driven & integrate servie

---
+++ frontend/naklario/src/app/landing/email-form/email-model.ts
@@ -0,0 +1,7 @@
+export class EmailModel {
+  constructor(
+    public email: string,
+    public ind_type: string,
+    public updates: boolean
+  ) {}
+}
--- frontend/naklario/src/app/landing/landing.component.ts
+++ frontend/naklario/src/app/landing/landing.component.ts
@@ -1,60 +1,12 @@
 import { Component, OnInit } from "@angular/core";
-import {
-  FormControl,
-  FormGroup,
-  FormBuilder,
-  Validators
-} from "@angular/forms";
-import { LandingService } from "./landing.service";
 
 @Component({
   selector: "app-landing",
   templateUrl: "./landing.component.html",
-  styleUrls: ["./landing.component.scss"],
-  providers: [LandingService]
+  styleUrls: ["./landing.component.scss"]
 })
 export class LandingComponent implements OnInit {
-  emailForm = new FormGroup({
-    type: new FormControl("", Validators.required),
-    email: new FormControl("", {
-      validators: [
-        Validators.required,
-        Validators.email,
-        Validators.maxLength(254)
-      ]
-    }),
-    updates: new FormControl(false)
-  });
-  emailFormSubmitted = false;
-
-  constructor(private landingService: LandingService, private fb: FormBuilder) {
-    this.onEmailFormSubmit();
-  }
-
-  onEmailFormSubmit(): void {
-    if (!this.emailForm.valid) {
-      return;
-    }
-    
-    const form = {
-      ind_type: this.emailForm.controls["type"].value,
-      email: this.emailForm.controls["email"].value,
-      updates: this.emailForm.controls["updates"].value
-    };
-    this.landingService.postForm(form).subscribe(
-      newForm => {
-        this.emailFormSubmitted = true;
-      },
-      // forward the error to the form
-      error => {
-        for (let [key, value] of Object.entries(error)) {
-          this.emailForm.controls[key].setErrors({
-            backend: (value as Array<string>).join(" ")
-          });
-        }
-      }
-    );
-  }
+  constructor() {}
 
   ngOnInit(): void {}
 }
