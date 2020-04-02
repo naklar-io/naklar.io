9b66621aeaa07489260d19c1d87563d373e6fe46
Max Fruehauf <fruehaufmaximilian@gmail.com>
2020-04-03 00:49:29 +0200

    link student register form

--- frontend/naklario/src/app/_helpers/form-validators.directive.ts
+++ frontend/naklario/src/app/_helpers/form-validators.directive.ts
@@ -1,16 +1,13 @@
-import {
-  ValidatorFn,
-  FormGroup,
-  ValidationErrors,
-} from "@angular/forms";
+import { ValidatorFn, FormGroup, ValidationErrors } from "@angular/forms";
 
 export const passwordNotMatchValidator: ValidatorFn = (
   control: FormGroup
 ): ValidationErrors | null => {
-
   const pw = control.get("password");
   const pwRepeat = control.get("passwordRepeat");
-  return pw && pwRepeat && pw.value !== pwRepeat.value
+  const notOk = pw.value && pwRepeat.value && pw.value !== pwRepeat.value;
+  //  console.log(notOk, pw, pwRepeat);
+  return notOk
     ? {
         passwordNotMatch: true
       }
--- frontend/naklario/src/app/account/student/student-register/student-register.component.ts
+++ frontend/naklario/src/app/account/student/student-register/student-register.component.ts
@@ -4,26 +4,41 @@ import {
   State,
   SchoolType,
   SchoolData,
-  User,
   states,
   schoolData,
-  schoolTypes
+  schoolTypes,
+  SendableUser
 } from "../../../_models/database";
+import { first } from "rxjs/operators";
 import { Options } from "ng5-slider";
+import { AuthenticationService } from "../../../_services";
+import { FormBuilder, Validators } from "@angular/forms";
+import { passwordNotMatchValidator } from "../../../_helpers";
 
 @Component({
   selector: "app-student-register",
   templateUrl: "./student-register.component.html",
-  styleUrls: ["./student-register.component.scss"]
+  styleUrls: ["./student-register.component.scss"],
+  providers: [AuthenticationService]
 })
 export class StudentRegisterComponent implements OnInit {
   states: State[] = states;
   schoolTypes: SchoolType[] = schoolTypes;
   schoolData: SchoolData[] = schoolData;
-  schoolType: number = -1;
-  grade: number = -1;
 
-  model = new User();
+  registerForm = this.fb.group(
+    {
+      firstName: ["", Validators.required],
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, Validators.minLength(8)]],
+      passwordRepeat: ["", [Validators.required, Validators.minLength(8)]],
+      state: [null, Validators.required],
+      schoolType: [null, Validators.required],
+      slider: [10, Validators.required],
+      terms: [false, Validators.requiredTrue]
+    },
+    { validators: passwordNotMatchValidator }
+  );
 
   // slider controls
   options: Options = {
@@ -34,11 +49,60 @@ export class StudentRegisterComponent implements OnInit {
   };
 
   submitted = false;
+  submitSuccess = false;
+  loading = false;
+  error: string = null;
 
-  constructor() {}
+  get f() {
+    return this.registerForm.controls;
+  }
+
+  constructor(
+    private fb: FormBuilder,
+    private authenticationService: AuthenticationService
+  ) {}
 
   onSubmit(): void {
     this.submitted = true;
+
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    const user: SendableUser = {
+      email: this.f.email.value,
+      password: this.f.password.value,
+      first_name: this.f.firstName.value,
+      last_name: "",
+      state: this.f.state.value.id,
+      terms_accepted: this.f.terms.value,
+      studentdata: {
+        school_data: schoolData.find(
+          x =>
+            x.grade === this.f.slider.value &&
+            x.school_type === this.f.schoolType.value.id
+        ).id
+      },
+      tutordata: null
+    };
+    console.log("About to send Data: ", user);
+
+    this.loading = true;
+    this.authenticationService
+      .register(user)
+      .pipe(first())
+      .subscribe(
+        data => {
+          // this.router.navigate([this.returnUrl]);
+          this.loading = false;
+          this.submitSuccess = true;
+          this.error = null;
+        },
+        error => {
+          this.error = error;
+          this.loading = false;
+        }
+      );
   }
 
   ngOnInit(): void {}
