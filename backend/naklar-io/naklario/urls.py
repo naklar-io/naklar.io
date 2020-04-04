19985178216e7b5d117ab2220a627b5b428d97aa
Korbinian Stein <kstein@inforbi.de>
2020-04-04 17:17:43 +0200

    Matching api and logic (#18)
    * Add API endpoints for creating/getting requests.
    
    * Breaking changes, work on match API
    
    * Fix format of roulette/serializers
    
    * Accept studentdata, tutordata on /account/create
    
    * Add feedback model
    
    * First version of matching logic.
    
    * Fix usercreation
    
    * Add new /swagger/ endpoint, finish matching logic

--- backend/naklar-io/naklario/urls.py
+++ backend/naklar-io/naklario/urls.py
@@ -1,35 +1,35 @@
-"""lernroulette URL Configuration The `urlpatterns` list routes URLs to views. For more information please see: https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 
 from django.urls import include, path
 from django.views.generic import TemplateView
 
-from rest_framework.schemas import get_schema_view
+from rest_framework import permissions
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
-# change this to view API
-TOKEN = "3232e8284a7ee9375b8f9d8eeb2ed297242d20c301a0d60b0f0ecbd57eae52a8"
+from knox import auth
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="naklario-API",
+      default_version='v1',
+      description="T",
+      terms_of_service="",
+      contact=openapi.Contact(email="info@naklar.io"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=False,
+   permission_classes=(permissions.AllowAny,),
+   authentication_classes=(auth.TokenAuthentication,)
+)
 urlpatterns = [
+    path('swagger?P<format>(\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('landing/', include('landing.urls')),
     path('account/', include('account.urls')),
+    path('roulette/', include('roulette.urls')),
     path('admin/', admin.site.urls),
-    path('openapi', get_schema_view(
-        title='naklar.io',
-        description='naklar.io - clientAPI',
-        version='0.1'), name='openapi-schema'),
-    path('swagger-ui/', TemplateView.as_view(
-        template_name='swagger-ui.html',
-        extra_context={'schema_url': 'openapi-schema',
-                       'token': TOKEN},
-    ), name='swagger-ui')]
+
+]
