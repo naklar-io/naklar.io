f1e3e369366671df19e1984319e39fefd7f0257c
Max Fruehauf <fruehaufmaximilian@gmail.com>
2020-04-07 16:08:34 +0200

    add lastName & small changes

--- frontend/naklario/src/app/account/student/student-register/student-register.component.ts
+++ frontend/naklario/src/app/account/student/student-register/student-register.component.ts
@@ -7,19 +7,19 @@ import {
   states,
   schoolData,
   schoolTypes,
-  SendableUser
+  SendableUser,
 } from "../../../_models/database";
 import { first } from "rxjs/operators";
 import { Options } from "ng5-slider";
 import { AuthenticationService } from "../../../_services";
 import { FormBuilder, Validators } from "@angular/forms";
 import { passwordNotMatchValidator } from "../../../_helpers";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: "app-student-register",
   templateUrl: "./student-register.component.html",
   styleUrls: ["./student-register.component.scss"],
-  providers: [AuthenticationService]
 })
 export class StudentRegisterComponent implements OnInit {
   states: State[] = states;
@@ -29,13 +29,14 @@ export class StudentRegisterComponent implements OnInit {
   registerForm = this.fb.group(
     {
       firstName: ["", Validators.required],
+      lastName: ["", Validators.required],
       email: ["", [Validators.required, Validators.email]],
       password: ["", [Validators.required, Validators.minLength(8)]],
       passwordRepeat: ["", [Validators.required, Validators.minLength(8)]],
       state: [null, Validators.required],
       schoolType: [null, Validators.required],
       slider: [10, Validators.required],
-      terms: [false, Validators.requiredTrue]
+      terms: [false, Validators.requiredTrue],
     },
     { validators: passwordNotMatchValidator }
   );
@@ -45,7 +46,7 @@ export class StudentRegisterComponent implements OnInit {
     animate: false,
     showTicks: true,
     floor: 5,
-    ceil: 13
+    ceil: 13,
   };
 
   submitted = false;
@@ -59,8 +60,12 @@ export class StudentRegisterComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute,
     private authenticationService: AuthenticationService
   ) {}
+
+  ngOnInit(): void {}
 
   onSubmit(): void {
     this.submitted = true;
@@ -73,37 +78,35 @@ export class StudentRegisterComponent implements OnInit {
       email: this.f.email.value,
       password: this.f.password.value,
       first_name: this.f.firstName.value,
-      last_name: "",
+      last_name: this.f.lastName.value,
       state: this.f.state.value.id,
       terms_accepted: this.f.terms.value,
       studentdata: {
-        school_data: schoolData.find(
-          x =>
+        school_data: this.schoolData.find(
+          (x) =>
             x.grade === this.f.slider.value &&
             x.school_type === this.f.schoolType.value.id
-        ).id
+        ).id,
       },
-      tutordata: null
+      tutordata: null,
     };
-    console.log("About to send Data: ", user);
+    console.log("About to send student Data: ", user);
 
     this.loading = true;
     this.authenticationService
       .register(user)
       .pipe(first())
       .subscribe(
-        data => {
-          // this.router.navigate([this.returnUrl]);
+        (data) => {
           this.loading = false;
           this.submitSuccess = true;
           this.error = null;
+          this.router.navigate(["/roulette/student"]);
         },
-        error => {
+        (error) => {
           this.error = error;
           this.loading = false;
         }
       );
   }
-
-  ngOnInit(): void {}
 }
