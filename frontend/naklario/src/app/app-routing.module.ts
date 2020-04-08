fb613ea471cd660b9721b1e058c127632d8ca7e2
Max Fruehauf <fruehaufmaximilian@gmail.com>
2020-04-08 21:12:04 +0200

    add terms and privacy texts

--- frontend/naklario/src/app/app-routing.module.ts
+++ frontend/naklario/src/app/app-routing.module.ts
@@ -4,19 +4,26 @@ import { Routes, RouterModule } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { PageNotFoundComponent } from "./_misc_components/page-not-found/page-not-found.component";
 import { LandingComponent } from "./landing/landing.component";
-import { ImpressumComponent } from "./_misc_components/impressum/impressum.component";
-import { AboutComponent } from "./about/about.component";
+import {
+  ImpressumComponent,
+  TermsConditionsComponent,
+} from "./_misc_components/";
+import { AboutComponent } from "./_misc_components/about/about.component";
 
 const routes: Routes = [
-  { path: "landing", component: LandingComponent },
-  { path: "impressum", component: ImpressumComponent },
+  { path: "terms", component: TermsConditionsComponent },
+  { path: "imprint", component: ImpressumComponent },
+  { path: "privacy", component: ImpressumComponent },
   { path: "about", component: AboutComponent },
+  { path: "landing", component: LandingComponent },
   { path: "", component: HomeComponent, pathMatch: "full" },
-  { path: "**", component: PageNotFoundComponent }
+  { path: "**", component: PageNotFoundComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { scrollPositionRestoration: "enabled" }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
