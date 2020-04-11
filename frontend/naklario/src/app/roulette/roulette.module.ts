cdf25652dde04680dce915aa6bf99bda36c939a7
Max Fruehauf <fruehaufmaximilian@gmail.com>
2020-04-11 19:14:33 +0200

    changes

--- frontend/naklario/src/app/roulette/roulette.module.ts
+++ frontend/naklario/src/app/roulette/roulette.module.ts
@@ -1,24 +1,30 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 
-import { RouletteRoutingModule } from './roulette-routing.module';
-import { ReactiveFormsModule } from '@angular/forms';
-import { RouletteComponent } from './roulette.component';
-import { StudentComponent } from './student/student.component';
-import { TutorComponent } from './tutor/tutor.component';
-import { Ng5SliderModule } from 'ng5-slider';
-import { WaitingComponent } from './waiting/waiting.component';
-import { FeedbackComponent } from './feedback/feedback.component';
-
-
+import { RouletteRoutingModule } from "./roulette-routing.module";
+import { ReactiveFormsModule } from "@angular/forms";
+import { RouletteComponent } from "./roulette.component";
+import { StudentComponent } from "./student/student.component";
+import { TutorComponent } from "./tutor/tutor.component";
+import { Ng5SliderModule } from "ng5-slider";
+import { WaitingComponent } from "./waiting/waiting.component";
+import { FeedbackComponent } from "./feedback/feedback.component";
+import { RouletteService } from "../_services";
 
 @NgModule({
-  declarations: [RouletteComponent, StudentComponent, TutorComponent, WaitingComponent, FeedbackComponent],
+  declarations: [
+    RouletteComponent,
+    StudentComponent,
+    TutorComponent,
+    WaitingComponent,
+    FeedbackComponent,
+  ],
   imports: [
     Ng5SliderModule,
     CommonModule,
     ReactiveFormsModule,
-    RouletteRoutingModule
-  ]
+    RouletteRoutingModule,
+  ],
+  providers: [RouletteService],
 })
-export class RouletteModule { }
+export class RouletteModule {}
--- frontend/naklario/src/app/roulette/tutor/tutor.component.ts
+++ frontend/naklario/src/app/roulette/tutor/tutor.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from "@angular/core";
+import { RouletteService, AuthenticationService } from "src/app/_services";
+import { MatchRequest, StudentRequest } from "src/app/_models";
 
 @Component({
-  selector: 'roulette-tutor',
-  templateUrl: './tutor.component.html',
-  styleUrls: ['./tutor.component.scss']
+  selector: "roulette-tutor",
+  templateUrl: "./tutor.component.html",
+  styleUrls: ["./tutor.component.scss"],
 })
 export class TutorComponent implements OnInit {
+  @Output() done = new EventEmitter<boolean>();
 
-  constructor() { }
+  constructor(
+    private rouletteService: RouletteService,
+    private authenticationService: AuthenticationService
+  ) {}
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  onSubmit() {
+    const subj = this.authenticationService.currentUserValue.tutordata
+      .subjects[0].id;
+    console.log(this.authenticationService.currentUserValue);
+    const request: StudentRequest = { subject: subj };
+    this.rouletteService.createRequest("tutor", request).subscribe(
+      (data) => {},
+      (error) => {}
+    );
   }
-
 }
