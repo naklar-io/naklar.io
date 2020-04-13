308f02cfdbe2aa85863f57adb7a09bf040e5f812
Max Fruehauf <fruehaufmaximilian@gmail.com>
2020-04-13 20:13:42 +0200

    add user card

---
+++ frontend/naklario/src/app/_misc_components/user-card/user-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit, Input } from "@angular/core";
+import { User } from "src/app/_models";
+
+@Component({
+  selector: "misc-user-card",
+  templateUrl: "./user-card.component.html",
+  styleUrls: ["./user-card.component.scss"],
+})
+export class UserCardComponent implements OnInit {
+  @Input() readonly user: User;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
