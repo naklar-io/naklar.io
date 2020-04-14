77e14d246245f5ffa06895dc2b1c8203c7f3a573
Max Fruehauf <fruehaufmaximilian@gmail.com>
2020-04-14 17:57:27 +0200

    add better error messages & fix navigation

---
+++ frontend/naklario/src/app/_helpers/account-type.guard.ts
@@ -0,0 +1,84 @@
+import { Injectable } from "@angular/core";
+
+import { AuthenticationService, ToastService } from "../_services";
+import {
+  CanActivate,
+  Router,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  CanActivateChild,
+} from "@angular/router";
+
+/**
+ * for sites which require a student account
+ */
+@Injectable({ providedIn: "root" })
+export class StudentGuard implements CanActivate, CanActivateChild {
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private toastService: ToastService
+  ) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const currentUser = this.authenticationService.currentUserValue;
+    if (currentUser) {
+      if (currentUser.studentdata) {
+        return true;
+      } else {
+        this.toastService.error(
+          "Du must einen Studenten Account haben, um hier hin zu kommen"
+        );
+      }
+    }
+    this.toastService.info("Bitte logge dich ein");
+    this.router.navigate(["account/login/"], {
+      queryParams: { returnUrl: state.url },
+    });
+    return false;
+  }
+
+  canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return this.canActivate(route, state);
+  }
+}
+
+/**
+ * for sites which require a verified tutor account
+ */
+@Injectable({ providedIn: "root" })
+export class TutorGuard implements CanActivate, CanActivateChild {
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService,
+    private toastService: ToastService
+  ) {}
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    const currentUser = this.authenticationService.currentUserValue;
+    if (currentUser) {
+      if (currentUser.tutordata) {
+        if (currentUser.tutordata.verified) {
+          return true;
+        } else {
+          this.toastService.error(
+            "Dein Account muss verifiziert sein, um hier hin zu kommen"
+          );
+        }
+      } else {
+        this.toastService.error(
+          "Du must einen Tutor Account haben, um hier hin zu kommen"
+        );
+      }
+    }
+    this.toastService.info("Bitte logge dich ein");
+    this.router.navigate(["account/login/"], {
+      queryParams: { returnUrl: state.url },
+    });
+    return false;
+  }
+
+  canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+    return this.canActivate(route, state);
+  }
+}
