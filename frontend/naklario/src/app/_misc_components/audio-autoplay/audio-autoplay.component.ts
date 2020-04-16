8abf87143c0534ea7ad11ffe759e5fe4db7c77e0
Max Fruehauf <fruehaufmaximilian@gmail.com>
2020-04-16 14:35:23 +0200

    add audio autoplaying when match is found

---
+++ frontend/naklario/src/app/_misc_components/audio-autoplay/audio-autoplay.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'misc-audio-autoplay',
+  templateUrl: './audio-autoplay.component.html',
+  styleUrls: ['./audio-autoplay.component.scss']
+})
+export class AudioAutoplayComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
