29846daba7f65e3c511ca187fc60d9f056378bb3
Korbinian Stein <kstein@inforbi.de>
2020-04-17 10:14:26 +0200

    Fix identation

--- backend/naklar-io/roulette/apps.py
+++ backend/naklar-io/roulette/apps.py
@@ -6,6 +6,7 @@ class RouletteConfig(AppConfig):
     name = 'roulette'
 
     def ready(self):
+        pass
 """         from . import jobs
         if os.environ.get('RUN_MAIN', None) != 'true':
             jobs.start_scheduler() """
