4cc60dced62d9745fa20c29844a4db33790b155b
Korbinian Stein <kstein@inforbi.de>
2020-04-17 09:33:59 +0200

    Delete requests unpolled for longer than 30 seconds (#50)

--- backend/naklar-io/roulette/apps.py
+++ backend/naklar-io/roulette/apps.py
@@ -1,5 +1,11 @@
 from django.apps import AppConfig
+import os
 
 
 class RouletteConfig(AppConfig):
     name = 'roulette'
+
+    def ready(self):
+        from . import jobs
+        if os.environ.get('RUN_MAIN', None) != 'true':
+            jobs.start_scheduler()
