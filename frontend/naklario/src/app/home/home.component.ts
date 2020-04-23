8078badce487bd45347aab8e77778030b131255b
Korbinian Stein <kstein@inforbi.de>
2020-04-23 14:10:27 +0200

    Add meta description and adjust title

--- frontend/naklario/src/app/home/home.component.ts
+++ frontend/naklario/src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { AuthenticationService } from "../_services";
 import { environment } from "src/environments/environment";
 import { User } from "../_models";
 import { Router } from "@angular/router";
+import { Meta, Title } from "@angular/platform-browser";
 
 @Component({
   selector: "app-home",
@@ -12,8 +13,20 @@ import { Router } from "@angular/router";
 export class HomeComponent implements OnInit {
   constructor(
     private authenticationService: AuthenticationService,
-    private router: Router
-  ) {}
+    private router: Router,
+    private meta: Meta,
+    private title: Title
+  ) {
+    this.meta.addTag({
+      name: "description",
+      content:
+        "Ehrenamtliche Tutoren geben Schüler*innen kostenlose Nachhilfe - ganz einfach und für jeden zugänglich.",
+    });
+    this.title.setTitle(
+      "naklar.io - Kostenlose Nachhilfe - Tutoren helfen ehrenamtlich"
+    );
+    this.meta.addTag({name: "keywords", content: "Kostenlose Nachhilfe Ehrenamtlich"});
+  }
 
   ngOnInit(): void {
     this.authenticationService.isLoggedIn$.subscribe((isLoggedIn) => {
