1635f14e0c48dd9dfa9a5c2c3c7170fab03bbb2b
Korbinian Stein <kstein@inforbi.de>
2020-05-08 11:19:51 +0200

    Fix angular tests (tests are failing still)

--- frontend/naklario/src/app/_misc_components/toasts/toasts.component.spec.ts
+++ frontend/naklario/src/app/_misc_components/toasts/toasts.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ToastsComponent } from './toasts-component';
+import { ToastsComponent } from './toasts.component';
 
 describe('AlertsComponent', () => {
   let component: ToastsComponent;
