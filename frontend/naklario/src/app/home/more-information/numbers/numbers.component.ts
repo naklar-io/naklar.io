35240f2c0766d82e6239e98f18145f36a1c429b1
Max Fruehauf <fruehaufmaximilian@gmail.com>
2020-05-18 21:36:51 +0200

    replace interval stacking with cubic spline interpolation

--- frontend/naklario/src/app/home/more-information/numbers/numbers.component.ts
+++ frontend/naklario/src/app/home/more-information/numbers/numbers.component.ts
@@ -1,48 +1,52 @@
-import { Component, Input, OnInit } from '@angular/core'
-import { BehaviorSubject, interval } from 'rxjs'
-import { take, takeWhile } from 'rxjs/operators'
+import { Component, Input, OnInit } from "@angular/core";
+import { BehaviorSubject, interval } from "rxjs";
+import { take, takeWhile, takeUntil, tap, map } from "rxjs/operators";
+import { MatchRequest } from "src/app/_models";
 
 @Component({
-  selector: 'home-more-information-numbers',
-  templateUrl: './numbers.component.html',
-  styleUrls: ['./numbers.component.scss']
+  selector: "home-more-information-numbers",
+  templateUrl: "./numbers.component.html",
+  styleUrls: ["./numbers.component.scss"],
 })
 export class NumbersComponent implements OnInit {
-  @Input() parentCurrentScrollSectionId: BehaviorSubject<string>
+  @Input() parentCurrentScrollSectionId: BehaviorSubject<string>;
 
-  userCounter = 0
+  userCounter = 0;
   // to be gotten externally in future
-  readonly USERS_COUNT = 3000
+  readonly USERS_COUNT = 3000;
 
-  constructor() {
-  }
+  constructor() {}
 
   ngOnInit(): void {
-    this.parentCurrentScrollSectionId.subscribe(id => this.onParentScrollSectionChange(id))
+    this.parentCurrentScrollSectionId.subscribe((id) =>
+      this.onParentScrollSectionChange(id)
+    );
   }
 
   onParentScrollSectionChange(id: string) {
-    if (id === 'numbers' && this.userCounter === 0) {
-      interval(30).pipe(take(106)).subscribe(() => this.userCounter += 19, () => {
-        },
-        () => interval(50).pipe(take(38)).subscribe(() => this.userCounter += 19, () => {
-          },
-          () => interval(70).pipe(takeWhile(() => this.userCounter < this.USERS_COUNT - 100)).subscribe(() => this.userCounter += 13, () => {
-            },
-            () => interval(80).pipe(take(11)).subscribe(() => this.userCounter += 6, () => {
-              },
-              () => interval(100).pipe(take(8)).subscribe(() => this.userCounter += 2, () => {
-                },
-                () => interval(120).pipe(takeWhile(() => this.userCounter + 1 < this.USERS_COUNT)).subscribe(() => this.userCounter += 1, () => {
-                  },
-                  () => this.userCounter = this.USERS_COUNT
-                )
-              )
-            )
-          )
-        )
-      )
+    const max = 3000;
+    // animation time in ms
+    const time = 2 * 1e3;
+    const fPrime0 = 7000;
+    // implementing cubic interpolation between (0,0) and (time, max)
+    const cubic = (t) => {
+      if (t <= 0) return 0;
+      if (t >= time) return max;
+      const a = -2 * max + fPrime0;
+      const b = 3 * max - 2 * fPrime0;
+      const c = fPrime0;
+      const d = 0;
+      t /= time;
+      return a * t ** 3 + b * t ** 2 + c * t + d;
+    };
+
+    if (id === "numbers" && this.userCounter === 0) {
+      // every 10ms send interval until
+      interval(10)
+        .pipe(take(time / 10))
+        // tick to ms
+        .pipe(map((tick) => Math.ceil(cubic((tick + 1) * 10))))
+        .subscribe((x) => (this.userCounter = x));
     }
   }
-
 }
