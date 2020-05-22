b94e3db2e2046e251ab7d1c0052a367bd69a6f08
Julius <julius.berger@gmail.com>
2020-05-22 13:25:47 +0200

    add "join the community" section

---
+++ frontend/naklario/src/app/home/more-information/join-the-community/join-the-community.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { JoinTheCommunityComponent } from './join-the-community.component';
+
+describe('JoinTheCommunityComponent', () => {
+  let component: JoinTheCommunityComponent;
+  let fixture: ComponentFixture<JoinTheCommunityComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ JoinTheCommunityComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(JoinTheCommunityComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
