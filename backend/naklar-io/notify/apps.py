bb449b2e0507df8eb13a8b010517c0af278e7698
Korbinian Stein <kstein@inforbi.de>
2020-06-03 23:46:45 +0200

    Cleanup

--- backend/naklar-io/notify/apps.py
+++ backend/naklar-io/notify/apps.py
@@ -7,8 +7,4 @@ class NotifyConfig(AppConfig):
     name = 'notify'
 
     def ready(self):
-       """ from post_office.models import EmailTemplate
-        if not EmailTemplate.objects.filter(name='notification').exists():
-            EmailTemplate.objects.create(
-                name='notification', content=DEFAULT_PUSH_TXT, html_content=DEFAULT_PUSH_HTML, subject="naklar.io - {{ subject }}", description="Standard-Template für PUSH-Notifications")
-""""
+        pass
