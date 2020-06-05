a20397485572c19aa72eeab2a2a0159ed939533b
Korbinian Stein <kstein@inforbi.de>
2020-06-05 13:17:59 +0200

    Optimizations for backend-matching

--- backend/naklar-io/roulette/matching.py
+++ backend/naklar-io/roulette/matching.py
@@ -6,7 +6,7 @@ from push_notifications.models import WebPushDevice
 from account.models import CustomUser
 from roulette.models import Match, StudentRequest, TutorRequest
 from django.db.models import F, Q
-from notify.models import Notification
+from notify.models import Notification, NotificationSettings
 
 
 def look_for_matches():
@@ -15,7 +15,7 @@ def look_for_matches():
     Might need significant performance improvements in the future (look at caching)
     """
 
-    student_rs = StudentRequest.objects.filter(is_active=True).filter(
+    student_rs = StudentRequest.objects.prefetch_related('failed_matches', 'user__studentdata').filter(is_active=True).filter(
         meeting__isnull=True).filter(match__isnull=True)
     for student_request in student_rs:
         student = student_request.user
@@ -58,37 +58,57 @@ def generate_notifications():
 
     # TODO: first step is to filter student requests that have no notifications sent yet?
     # next would be, filter only the ones that havent sent notifications in X seconds
-    student_rs = student_rs.filter(notifications=None)
+    student_rs = student_rs.filter(Q(notifications__isnull=True) |
+                                   Q(notifications__date__lte=timezone.now() - timedelta(seconds=10))).prefetch_related('user', 'subject', 'notifications').distinct()
+    # TODO: prioritize those with longer waiting times
+    # student_rs = student_rs.order_by(
+    # fetch possible tutors beforehand, reducing the query complexity each step
+
+    # Find matching tutors
+    # this may need to be optimized further --> Selecting whole user table!
+    all_tutors = CustomUser.objects.prefetch_related('tutordata').filter(
+        tutordata__isnull=False, email_verified=True, tutordata__verified=True,
+        notificationsettings__isnull=False)
+
+    # find those who have notifications turned on and interval is okay
+    all_tutors = all_tutors.filter(
+        Q(notificationsettings__enable_push=True) | Q(
+            notificationsettings__enable_mail=True)).exclude(
+        notification__date__gte=timezone.now() - F('notificationsettings__notify_interval')).distinct()
+
+    time_range_query = Q(notificationsettings__ranges__days__contains=timezone.datetime.today().weekday(),
+                         notificationsettings__ranges__start_time__lte=timezone.now().time(),
+                         notificationsettings__ranges__end_time__gte=timezone.now().time())
+
+    # now to find those whose time is in range
+    all_tutors = all_tutors.filter((Q(notificationsettings__ranges_mode=NotificationSettings.RANGE_ALLOW) & time_range_query) | Q(
+        Q(notificationsettings__ranges_mode=NotificationSettings.RANGE_BLOCK) & ~time_range_query)).distinct()
+
+    # don't do anything if there arent any student requests
+    if not student_rs:
+        return
+    # all_tutors = list(all_tutors)
     for request in student_rs:
-        # Find matching tutors
-        tutors = CustomUser.objects.filter(
-            tutordata__isnull=False, email_verified=True, tutordata__verified=True,
-            notificationsettings__isnull=False)
 
-        # find those who have notifications turned on and interval is okay
-        tutors = tutors.filter(
-            Q(notificationsettings__enable_push=True) | Q(notificationsettings__enable_mail=True),
-            notification__date__lte=timezone.now() - F('notificationsettings__notify_interval')).distinct()
+        # filter again for those who didn't already get a notification for the same request
+        tutors = all_tutors.filter(tutordata__subjects=request.subject).exclude(
+            notification__in=request.notifications.all())
+            #.exclude(notification__date__gte=timezone.now() - F('notificationsettings__notify_interval')).distinct()
 
-        # now to find those whose time is in range
-        tutors = tutors.filter(
-            notificationsettings__ranges__days__contains=timezone.datetime.today().weekday(),
-            notificationsettings__ranges__start_time__lte=timezone.now().time(),
-            notificationsettings__ranges__end_time__gte=timezone.now().time()
-        ).distinct()
         # now to find the 10 best!
-        sorted_tutors = sorted(tutors, key=lambda tutor: calculate_user_matching_score(request, tutor))[::-1][:10]
+        sorted_tutors = sorted(tutors, key=lambda tutor: calculate_user_matching_score(
+            request, tutor))[::-1][:10]
         for t in sorted_tutors:
             notification = Notification()
             notification.user = t
+            notification.type = Notification.STUDENT_REQUEST
             notification.title = f"{request.user.first_name} braucht deine Hilfe!"
             notification.body = f"Fach: {request.subject.name} in {str(request.user.studentdata.school_data)}"
             notification.content_object = request
             notification.save()
             notification.send()
 
-            # notification.body = 
-
+            # notification.body =
 
 
 def calculate_request_matching_score(student_request: StudentRequest, tutor_request: TutorRequest):
