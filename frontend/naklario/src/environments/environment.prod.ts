85a337428762c784e209d5e10767ae7c2aa9be9e
Korbinian Stein <kstein@inforbi.de>
2020-08-07 14:41:53 +0200

    fix prod apiUrl

--- frontend/naklario/src/environments/environment.prod.ts
+++ frontend/naklario/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "http://192.168.2.106:8000",
+  apiUrl: "https://api.naklar.io",
   features: { roulette: true },
   vapidKey: "BNoY3a9ohX5PTkDzqpsq_6LifNMwNpm0J6jTZyG-Xv63z8CkiQT7mi3EDGCky5txmRuKmFtyFSugWgZXbXXxWXg"
 };
