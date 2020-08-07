448cb32157a928dbf76d26c0c9ae693ebf3b0742
Korbinian Stein <kstein@inforbi.de>
2020-08-07 14:32:16 +0200

    Configuration and build changes

--- backend/naklar-io/naklario/settings_local.py
+++ backend/naklar-io/naklario/settings_local.py
@@ -3,21 +3,21 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-BBB_SHARED = "CHANGETHIS" # TODO
+BBB_SHARED = "CHANGETHIS"  # TODO
 
 # HOW TO GET KEY:
 #   from django.core.management.utils import get_random_secret_key
 #   get_random_secret_key()
-SECRET_KEY = "CHANGETHIS" # TODO
+SECRET_KEY = "CHANGETHIS"  # TODO
 
 
+API_HOST = "https://dev.api.naklar.io"  # TODO
+HOST = "https://dev.naklar.io"  # TODO
 
-API_HOST = "https://dev.api.naklar.io" # TODO
-HOST = "https://dev.naklar.io" # TODO
-
-# TODO 
+# TODO
 CORS_ORIGIN_WHITELIST = [
-    "http://localhost:4000",
+    "http://localhost:8081",
+    "http://localhost:4200",
 ]
 
 # TODO
@@ -37,8 +37,12 @@ EMAIL_PORT = "1025"
 # TODO
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dev',
+        'USER': 'dev',
+        'PASSWORD': 'dev',
+        'HOST': 'localhost',
+        'PORT': 5432,
     }
 }
 
@@ -48,3 +52,5 @@ WEBPUSH_SETTINGS = {
     "VAPID_PRIVATE_KEY": "Private Key",
     "VAPID_ADMIN_EMAIL": "admin@example.org"
 }
+
+DEBUG = True
