b8c9dadd19f7ec50df310ca50e9b90bdfa30242f
Korbinian Stein <kstein@inforbi.de>
2020-08-09 12:54:33 +0200

    Add summer pause modals.

--- frontend/naklario/src/app/dashboard/dashboard.component.ts
+++ frontend/naklario/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,17 @@
-import { Component, OnInit, AfterViewChecked, AfterViewInit } from "@angular/core";
-import { AuthenticationService, AccountType, BannerService } from "../_services";
+import {
+  Component,
+  OnInit,
+  AfterViewChecked,
+  AfterViewInit,
+} from "@angular/core";
+import {
+  AuthenticationService,
+  AccountType,
+  BannerService,
+} from "../_services";
+import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { PauseModalComponent } from '../roulette/pause-modal/pause-modal.component';
 
 @Component({
   selector: "app-dashboard",
@@ -9,16 +21,28 @@ import { AuthenticationService, AccountType, BannerService } from "../_services"
 export class DashboardComponent implements OnInit {
   accountType: AccountType;
 
-  constructor(private authenticationService: AuthenticationService, private bannerService: BannerService
-    ) {}
+  constructor(
+    private authenticationService: AuthenticationService,
+    private bannerService: BannerService,
+    private modalService: NgbModal,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     this.authenticationService.getAccountType().subscribe((t) => {
       this.accountType = t;
     });
     var hours = new Date().getUTCHours();
-    if (hours < 8 || (hours > 10 && hours < 13) || (hours >= 15)) {
+    if (hours < 8 || (hours > 10 && hours < 13) || hours >= 15) {
       this.bannerService.showBanner();
     }
+  }
+
+  startTutorMatching(): void {
+    this.modalService.open(PauseModalComponent).result.then((result) => {
+      this.router.navigateByUrl("/roulette/tutor");
+    }, (reason) => {
+      console.log("dismissed");
+    })
   }
 }
