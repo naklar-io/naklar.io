38f5700673b9da6630fed7373a92ab12eab9e15f
Korbinian Stein <kstein@inforbi.de>
2020-08-31 12:47:03 +0200

    Reduce database fetching and log spam

--- frontend/naklario/src/app/_services/database.service.ts
+++ frontend/naklario/src/app/_services/database.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { Observable, throwError, combineLatest } from "rxjs";
 import { environment } from "../../environments/environment";
-import { catchError, map, tap } from "rxjs/operators";
+import { catchError, map, tap, shareReplay } from "rxjs/operators";
 import { of } from "rxjs";
 import {
   State,
@@ -22,6 +22,8 @@ export class DatabaseService {
   public schoolTypes: Observable<SchoolType[]>;
   public schoolData: Observable<SchoolData[]>;
   public genders: Observable<Gender[]>;
+
+  private cache = {};
 
   constructor(private http: HttpClient) {
     this.states = this.get<State[]>("/account/states/");
@@ -44,12 +46,20 @@ export class DatabaseService {
   }
 
   private get<T>(url: string): Observable<T> {
-    return this.http.get<T>(environment.apiUrl + url).pipe(
+    if (this.cache[url]) {
+      console.log("using cached data");
+      return this.cache[url];
+    }
+    console.log("do request again");
+    this.cache[url] = this.http.get<T>(environment.apiUrl + url).pipe(
+      shareReplay(1),
       catchError((err) => {
         console.log("Database request error: ", err);
+        delete this.cache[url]
         return throwError(err);
       })
     );
+    return this.cache[url];
   }
 
   public getConstants() {
