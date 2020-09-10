7c9f31f2c3f9a2ae1a51a8ed6f3dea0371148db8
Korbinian Stein <kstein@inforbi.de>
2020-09-10 12:56:49 +0200

    Add redis for channel layers

--- backend/naklar-io/naklario/routing.py
+++ backend/naklar-io/naklario/routing.py
@@ -1,7 +1,15 @@
+
 from channels.routing import ProtocolTypeRouter, URLRouter
 
 import roulette.routing
+from django.urls import include, path
+
+socketroutes = [
+    path("roulette", URLRouter(roulette.routing.urlpatterns))
+]
 
 application = ProtocolTypeRouter({
-    'http': URLRouter(roulette.routing.urlpatterns),
+    # 'http': URLRouter(roulette.routing.urlpatterns),
+    'websocket': URLRouter(socketroutes)
 })
+
