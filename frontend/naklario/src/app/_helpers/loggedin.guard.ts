40c3545b5e6f3f87460b09705c37bceacc00cdd7
Korbinian Stein <kstein@inforbi.de>
2020-09-27 20:11:01 +0200

    Fix logged-in-guard if already logged in

--- frontend/naklario/src/app/_helpers/loggedin.guard.ts
+++ frontend/naklario/src/app/_helpers/loggedin.guard.ts
@@ -6,7 +6,7 @@ import {
   Router,
   ActivatedRouteSnapshot,
   RouterStateSnapshot,
-  CanActivateChild,
+  CanActivateChild, UrlTree
 } from '@angular/router';
 
 @Injectable({ providedIn: 'root' })
@@ -15,7 +15,7 @@ export class LoggedInGuard implements CanActivate, CanActivateChild {
     private router: Router,
     private authenticationService: AuthenticationService,
     private toastService: ToastService
-  ) {}
+  ) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     if (this.authenticationService.isLoggedIn) {
@@ -39,17 +39,20 @@ export class NotLoggedInGuard implements CanActivate, CanActivateChild {
     private router: Router,
     private authenticationService: AuthenticationService,
     private toastService: ToastService
-  ) {}
+  ) { }
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
     if (!this.authenticationService.isLoggedIn) {
       return true;
     } else {
-      this.toastService.info('Bitte logge dich ein');
-      this.router.navigate(['account/login'], {
-        queryParams: { returnUrl: state.url },
-      });
-      return false;
+      this.toastService.error('Du bist bereits registriert und eingeloggt!');
+      // Check if this is the initial page load. If yes, we need to redirect to a different page. In this case, the current profile
+      if (!this.router.navigated) {
+        const tree: UrlTree = this.router.parseUrl('/account');
+        return tree;
+      } else {
+        return false;
+      }
     }
   }
   canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
