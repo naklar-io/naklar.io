54f34494c0370bf72bbf39b4e1327818ede66bac
Korbinian Stein <kstein@inforbi.de>
2020-09-27 21:00:57 +0200

    removing buggy "type" annotation

--- backend/naklar-io/roulette/urls.py
+++ backend/naklar-io/roulette/urls.py
@@ -4,13 +4,10 @@ from drf_yasg.utils import swagger_auto_schema
 
 from . import views
 
-type_parameter = openapi.Parameter('type', openapi.IN_PATH, description='User type',
-                                   required=True, type=openapi.TYPE_STRING, enum=['student', 'tutor'])
 app_name = 'roulette'
 
 typed_patterns = [
-    path('request/', swagger_auto_schema(manual_parameters=[type_parameter], methods=[
-         'POST', 'DELETE', 'GET'])(views.RequestView.as_view())),
+    path('request/', views.RequestView.as_view()),
     #path('request/delete/', swagger_auto_schema(manual_parameters=[type_parameter], method='DELETE')(views.RequestDeleteView.as_view())),
     #path('request/create/', swagger_auto_schema(manual_parameters=[type_parameter], method='POST')(views.RequestCreateView.as_view())),
     path('match/answer/<uuid:uuid>/', views.match_answer),
