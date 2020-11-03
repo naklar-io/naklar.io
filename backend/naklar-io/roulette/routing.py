622127de00ca70bf1dfc0d6fc07d65852599aca4
Korbinian Stein <kstein@inforbi.de>
2020-11-03 20:47:34 +0100

    Update for channels 3.0

--- backend/naklar-io/roulette/routing.py
+++ backend/naklar-io/roulette/routing.py
@@ -6,5 +6,5 @@ from django.urls import path
 from roulette import consumers
 
 urlpatterns = [
-    path("request/<str:type>/<int:request_id>", consumers.RouletteConsumer),
+    path("request/<str:type>/<int:request_id>", consumers.RouletteConsumer.as_asgi()),
 ]
