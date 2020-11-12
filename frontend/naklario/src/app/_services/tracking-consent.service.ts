19419b5106592c9b87c4017b14ee41ce541b2318
Korbinian Stein <kstein@inforbi.de>
2020-11-12 17:27:44 +0100

    Update to be more extensible

--- frontend/naklario/src/app/_services/tracking-consent.service.ts
+++ frontend/naklario/src/app/_services/tracking-consent.service.ts
@@ -1,35 +1,38 @@
 import { isPlatformBrowser } from '@angular/common';
 import { Inject, Injectable, PLATFORM_ID } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { TrackingConsentSettings } from '../_models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TrackingConsentService {
 
-  TRACKING_KEY = 'trackingAllowed';
-  private allowTracking = new BehaviorSubject(false);
-  public allowTracking$ = this.allowTracking.asObservable();
+  TRACKING_KEY = 'trackingSettings';
+  private trackingSettings: BehaviorSubject<TrackingConsentSettings> = new BehaviorSubject({
+    googleAnalytics: false
+  });
+  public trackingSettings$ = this.trackingSettings.asObservable();
 
   constructor(
     @Inject(PLATFORM_ID) private platformId,
   ) {
     if (isPlatformBrowser(platformId)) {
       const value = JSON.parse(localStorage.getItem(this.TRACKING_KEY));
-      this.allowTracking.next(value);
-      this.allowTracking$.subscribe((allowValue) => {
+      this.trackingSettings.next(value);
+      this.trackingSettings$.subscribe((allowValue) => {
         localStorage.setItem(this.TRACKING_KEY, JSON.stringify(allowValue));
       });
     }
   }
 
-  public enableTracking(): void {
-    this.allowTracking.next(true);
+  public changeTrackingSettings(settings: TrackingConsentSettings): void {
+    this.trackingSettings.next(settings);
   }
 
   public disableTracking(): void {
-    const shouldReload = this.allowTracking.value;
-    this.allowTracking.next(false);
+    const shouldReload = this.trackingSettings.value.googleAnalytics;
+    this.trackingSettings.next({googleAnalytics: false});
     if (shouldReload) {
       try {
         window.location.reload();
