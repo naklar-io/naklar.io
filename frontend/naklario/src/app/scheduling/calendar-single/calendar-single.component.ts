7c36e73815d88ce52a9736b6aacc93506d45618e
juli303 <julian@mineguild.net>
2020-12-09 16:42:14 +0100

    NAK-147

--- frontend/naklario/src/app/scheduling/calendar-single/calendar-single.component.ts
+++ frontend/naklario/src/app/scheduling/calendar-single/calendar-single.component.ts
@@ -1,22 +1,50 @@
-import { ChangeDetectionStrategy, Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { CalendarEvent, CalendarView } from 'angular-calendar';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  OnInit,
+  ViewEncapsulation,
+} from "@angular/core";
+import {
+  CalendarEvent,
+  CalendarView,
+  CalendarWeekViewBeforeRenderEvent,
+} from "angular-calendar";
 
 @Component({
-  selector: 'scheduling-calendar-single',
+  selector: "scheduling-calendar-single",
   changeDetection: ChangeDetectionStrategy.OnPush,
-  templateUrl: './calendar-single.component.html',
-  styleUrls: ['./calendar-single.component.scss'],
+  templateUrl: "./calendar-single.component.html",
+  styleUrls: ["./calendar-single.component.scss"],
   encapsulation: ViewEncapsulation.None,
 })
 export class CalendarSingleComponent implements OnInit {
-
   public viewDate: Date = new Date();
   public events: CalendarEvent[] = [];
 
-  constructor() { }
+  clickedDate: Date;
+
+  
 
 
-  ngOnInit(): void {
+  //Hier wird gerendert welche Slots verfügbar sind und welche nicht
+  beforeWeekViewRender(renderEvent: CalendarWeekViewBeforeRenderEvent) {
+    renderEvent.hourColumns.forEach((hourColumn) => {
+      hourColumn.hours.forEach((hour) => {
+        hour.segments.forEach((segment) => {
+          if (
+            (segment.date.getHours() >= 14 ||
+            segment.date.getHours() <= 10) &&
+            segment.date.getDay() === 2
+            || segment.date.getDay() != 2
+          ) {
+            segment.cssClass = 'bg-disabled';
+          }
+        });
+      });
+    });
   }
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
