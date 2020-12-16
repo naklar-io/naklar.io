a5e0433fea6721d0142fdda7cf9f3db105042f26
juli303 <julian@mineguild.net>
2020-12-16 14:37:07 +0100

    added Terminansicht-component NAK-146

--- frontend/naklario/src/app/scheduling/scheduling-routing.module.ts
+++ frontend/naklario/src/app/scheduling/scheduling-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CalendarSingleComponent } from './calendar-single/calendar-single.component';
+import { MeetingCardComponent } from './meeting-card/meeting-card.component';
 
 const routes: Routes = [
   {
     path: 'test',
-    component: CalendarSingleComponent
+    component: MeetingCardComponent
   },
 ]
 
