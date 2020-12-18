c20de9f3eb27653b5b8bc427c7f2c5630b7315bf
juli303 <julirix@hotmail.de>
2020-12-18 09:53:32 +0100

    added Meeting-Card to Dashboards

--- frontend/naklario/src/app/scheduling/meeting-card/meeting-card.component.ts
+++ frontend/naklario/src/app/scheduling/meeting-card/meeting-card.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { environment } from "src/environments/environment";
 
 @Component({
-  selector: 'scheduling-meeting-card',
-  templateUrl: './meeting-card.component.html',
-  styleUrls: ['./meeting-card.component.scss']
+  selector: "scheduling-meeting-card",
+  templateUrl: "./meeting-card.component.html",
+  styleUrls: ["./meeting-card.component.scss"],
 })
 export class MeetingCardComponent implements OnInit {
+  //featureEnabled = environment.features.scheduling;
+  featureEnabled = true;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
