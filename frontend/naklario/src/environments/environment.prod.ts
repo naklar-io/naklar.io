431cb5acf2b8e56b9d3ca49d6cabfc52859e2997
Korbinian Stein <kstein@inforbi.de>
2021-01-02 18:37:48 +0100

    Enable analytics for frontend

--- frontend/naklario/src/environments/environment.prod.ts
+++ frontend/naklario/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     apiUrl: 'https://api.sachsen.naklar.io',
-    features: { roulette: true, notifications: false, analytics: false, codes: true },
+    features: { roulette: true, notifications: false, analytics: true, codes: true },
     vapidKey:
         'BNoY3a9ohX5PTkDzqpsq_6LifNMwNpm0J6jTZyG-Xv63z8CkiQT7mi3EDGCky5txmRuKmFtyFSugWgZXbXXxWXg',
 };
